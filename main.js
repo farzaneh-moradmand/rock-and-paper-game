dc6080acab2bda246aaeb0cfbfe51fb76ab61ecd
Farzaneh Moradmand <farizadm2@gmail.com>
2022-05-24 19:20:57 +0430

    calculator

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
-const choices = ["rock", "paper", "scissors"];
-const userChoice = prompt("please choose rock or paper or scissors");
-if (userChoice) {
-    console.log(`You choose ${userChoice} `);
-}else {
-    console.log("You cheated");
-}
-const randomNumber = Math.floor(Math.random() * 3);
-const computerChoice = choices[randomNumber];
-console.log(`Computer chooses: ${computerChoice}`);
+let displayBox = document.querySelector(".display");
 
-if (userChoice === computerChoice) {
-    console.log("it is a tie");
-}else if (userChoice === "rock") {
-    if (computerChoice === "scissors") {
-        console.log ("You win!");
-    } else {
-        console.log("You lose!");
-    }
-}else if (userChoice === "paper") {
-    if (computerChoice === "rock") {
-        console.log ("You win!");
-    } else {
-        console.log("You lose!");
-    }
-} else if (userChoice === "scissors") {
-    if (computerChoice === "paper") {
-        console.log ("You win!");
-    } else {
-        console.log("You lose!");
-    }
-}  
+
+let list = document.querySelectorAll(".show-display");
+console.log(list);
+//     list.forEach(item => function(item){
+
+//     item.addEventListener("click" , function(event)){
+//         console.log("click")
+
+//     }
+// })
 
